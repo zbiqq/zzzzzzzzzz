5d27eb7d68f30b8ea53a47f2522ae49a452ad023
zbiqq <mich.zbiq@gmail.com>
2024-10-12 18:18:52 +0200

    Hello world

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        //   System.out.println("Hello world");
+//        int inputNumber = 9;
+ //       int testNumber = 9;
 
-        System.out.println("Hello world");
+ //       if (inputNumber > testNumber) {
+  //          System.out.println("Liczba " + inputNumber + " jest większa od liczby " + testNumber);
+  //      } else if (inputNumber == testNumber) {
+   //         System.out.println("Liczby są sobie równe");
+   //     } else {
+   //         System.out.println("Liczba " + inputNumber + " jest mniejsza od liczby " + testNumber);
+    //
+        for (int i = 0; i < 10; i++) {
+            System.out.print((i + 1) + ", ");
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę: ");
+        int inputNumber = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Podana liczba to: " + inputNumber);
     }
 }
